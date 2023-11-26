1531f61de7856257f436ab950279e166313c7f16
Italo <italodopeey@gmail.com>
2023-11-26 11:32:57 -0300

    refactor: eslint-disable-next-line @typescript-eslint/no-unused-vars

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,15 +9,18 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.use((err: Error, request: Request, reply: Response, next: NextFunction) => {
-  if (err instanceof Error) {
-    return reply.status(400).json({ error: err.message })
-  }
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, _request: Request, reply: Response, _next: NextFunction) => {
+    if (err instanceof Error) {
+      return reply.status(400).json({ error: err.message })
+    }
 
-  return reply
-    .status(500)
-    .json({ status: 'error', message: 'Internal server error.' })
-})
+    return reply
+      .status(500)
+      .json({ status: 'error', message: 'Internal server error.' })
+  },
+)
 
 app.listen(3333, () =>
   console.log('🚀 HTTP Server running an http://localhost:3333'),
