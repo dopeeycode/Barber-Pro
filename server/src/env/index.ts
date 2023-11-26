b1e24f6f733de883ee440ec0ad121186c5800060
Italo <italodopeey@gmail.com>
2023-11-26 12:20:00 -0300

    chore: ensuring create environment variables

---
+++ server/src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  JWT_SECRET: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('😢 Invalid environment variables!', _env.error.format())
+
+  throw new Error('😢 Invalid environment variables.')
+}
+
+export const env = _env.data
