4efcfcf6575abbec13c3ecbd583442bfbd172a53
Italo <italodopeey@gmail.com>
2023-11-26 10:28:38 -0300

    feat: handling errors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
-import express from 'express'
+import cors from 'cors'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
+
+app.use((err: Error, request: Request, reply: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return reply.status(400).json({ error: err.message })
+  }
+
+  return reply
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error.' })
+})
 
 app.listen(3333, () =>
   console.log('🚀 HTTP Server running an http://localhost:3333'),
